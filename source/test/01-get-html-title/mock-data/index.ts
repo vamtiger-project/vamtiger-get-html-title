ad864b97e56aabbc7df57defa3204662a0ccf1f3
James-Alex Matthews <vamtiger@vamtiger.com>
2018-05-26 12:04:20 +0200

    vamtiger-node-typescript-commit: Updated ReadMe

--- source/test/01-get-html-title/mock-data/index.ts
+++ source/test/01-get-html-title/mock-data/index.ts
@@ -4,11 +4,11 @@ export default `<!DOCTYPE html>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>
-        Some HTML Title
-    </title>
+    <title>Some HTML Title</title>
 </head>
 <body>
-    
+    <div>
+        Some HTML body inner HTML
+    </div>
 </body>
 </html>`;
