18ab6c97974f385c0e1cadb2d6a70bba58905a1b
James-Alex Matthews <vamtiger@vamtiger.com>
2018-05-26 12:04:24 +0200

    vamtiger-node-typescript-commit: Updated ReadMe

--- build/test/01-get-html-title/mock-data/index.d.ts
+++ build/test/01-get-html-title/mock-data/index.d.ts
@@ -1,2 +1,2 @@
-declare const _default: "<!DOCTYPE html>\n<html lang=\"en\">\n<head>\n    <meta charset=\"UTF-8\">\n    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n    <meta http-equiv=\"X-UA-Compatible\" content=\"ie=edge\">\n    <title>\n        Some HTML Title\n    </title>\n</head>\n<body>\n    \n</body>\n</html>";
+declare const _default: "<!DOCTYPE html>\n<html lang=\"en\">\n<head>\n    <meta charset=\"UTF-8\">\n    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n    <meta http-equiv=\"X-UA-Compatible\" content=\"ie=edge\">\n    <title>Some HTML Title</title>\n</head>\n<body>\n    <div>\n        Some HTML body inner HTML\n    </div>\n</body>\n</html>";
 export default _default;
